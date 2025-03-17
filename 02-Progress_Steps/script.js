a0c3f311c0a43e4e03f0cb3cb76ce0545fcd0f50
Yichen Tian <tim@Mac-2.modem>
2025-03-17 22:08:36 +1030

    Fix display error

--- 02-Progress_Steps/script.js
+++ 02-Progress_Steps/script.js
@@ -8,17 +8,17 @@ let currentActive = 1
 next.addEventListener('click', () => {
     currentActive++
 
-    if(currentActive > circles.length) {
+    if (currentActive > circles.length) {
         currentActive = circles.length
     }
 
-    PaymentRequestUpdateEvent()
+    update()
 })
 
 prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive < 1) {
+    if (currentActive < 1) {
         currentActive = 1
     }
 
@@ -27,7 +27,7 @@ prev.addEventListener('click', () => {
 
 function update() {
     circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
+        if (idx < currentActive) {
             circle.classList.add('active')
         } else {
             circle.classList.remove('active')
@@ -38,9 +38,9 @@ function update() {
 
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
-    if(currentActive === 1) {
+    if (currentActive === 1) {
         prev.disabled = true
-    } else if(currentActive === circles.length) {
+    } else if (currentActive === circles.length) {
         next.disabled = true
     } else {
         prev.disabled = false
