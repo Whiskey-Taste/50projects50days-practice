2f39c0e54ba128401ee9460faddef32af374a11b
Tim Tian <timtian@Tims-MacBook-Air.local>
2025-03-24 14:13:52 +1030

    Fix bugs and create README.md

--- 04-Hidden_Search_widget/script.js
+++ 04-Hidden_Search_widget/script.js
@@ -2,7 +2,7 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
-btn.addEventListener('Click', () => {
+btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
